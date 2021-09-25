2ab7912433d02ab0a60a0402819e334d3589d9e1
Ali Aydemir <ali.aydemir93@hotmail.com>
2021-09-25 03:30:39 +0300

    added webpack, babel, eslint

---
+++ webpack.dev.config.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    entry: './src/js/app.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './dist'),
+        publicPath: ''
+    },
+    mode: 'development',
+    devServer: {
+        port: 9000,
+        static: {
+            directory: path.resolve(__dirname, './dist'),
+            watch: true
+        },
+        devMiddleware: {
+            index: 'index.html',
+            writeToDisk: true
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpg)$/,
+                type: 'asset/resource',
+                loader: 'file-loader',
+                options: {
+                    outputPath: 'img',
+                }
+            },
+            {
+                test: /\.txt/,
+                type: 'asset/source'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader', 'css-loader'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader', 'css-loader', 'sass-loader'
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    // loader: 'babel-loader',
+                    use: ['babel-loader', 'eslint-loader'],
+                    exclude: /node_modules/,
+                    options: {
+                        presets: ['@babel/env'],
+                        plugins: ['@babel/plugin-proposal-class-properties']
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Pocial',
+            template: 'src/index.html'
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: "./src/img", to: path.resolve(__dirname, './dist/img') }
+            ],
+        })
+    ]
+};
---
+++ webpack.production.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/js/app.js',
+    output: {
+        filename: 'bundle.[contenthash].js',
+        path: path.resolve(__dirname, './dist'),
+        publicPath: ''
+    },
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /img\.(png|jpg)$/,
+                type: 'asset'
+            },
+            {
+                test: /\.txt/,
+                type: 'asset/source'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader, 'css-loader'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    // loader: 'babel-loader',
+                    use: ['babel-loader', 'eslint-loader'],
+                    exclude: /node_modules/,
+                    options: {
+                        presets: ['@babel/env'],
+                        plugins: ['@babel/plugin-proposal-class-properties']
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'style.[contenthash].css'
+        }),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Pocial',
+            template: 'src/index.html'
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: "./src/img", to: path.resolve(__dirname, './dist/img') }
+            ],
+        })
+    ]
+};
